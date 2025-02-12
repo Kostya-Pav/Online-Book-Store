d8897ea92118497dad6028cf713997c8fd510a93
Костя <koast59385@gmail.com>
2025-02-12 21:07:49 +0200

    Add corrections

--- backend/src/main/java/com/onlinebookstore/service/BookServiceImpl.java
+++ backend/src/main/java/com/onlinebookstore/service/BookServiceImpl.java
@@ -3,17 +3,14 @@ package com.onlinebookstore.service;
 import com.onlinebookstore.model.Book;
 import com.onlinebookstore.repository.BookRepository;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
-
-    @Autowired
-    public BookServiceImpl(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
 
     @Override
     public Book save(Book book) {
