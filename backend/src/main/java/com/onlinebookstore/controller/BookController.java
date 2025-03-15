cb645978326f79639860185163a17e3800b4bc35
Kostya-Pav <koast59385@gmail.com>
2025-03-16 00:37:20 +0200

    add corrections

--- backend/src/main/java/com/onlinebookstore/controller/BookController.java
+++ backend/src/main/java/com/onlinebookstore/controller/BookController.java
@@ -1,9 +1,12 @@
 package com.onlinebookstore.controller;
 
+import static org.springframework.data.util.ReflectionUtils.setField;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.util.ReflectionUtils.getField;
+import static org.springframework.util.ReflectionUtils.makeAccessible;
 
 import com.onlinebookstore.dto.BookResponse;
 import com.onlinebookstore.dto.CreateBookRequest;
@@ -12,7 +15,9 @@ import com.onlinebookstore.model.Book;
 import com.onlinebookstore.model.SearchParameters;
 import com.onlinebookstore.service.BookService;
 import jakarta.persistence.EntityNotFoundException;
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.function.Consumer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -43,7 +48,7 @@ public class BookController {
     @GetMapping("/{id}")
     public ResponseEntity<BookResponse> getById(@PathVariable("id") Long id) {
         try {
-            BookResponse bookResponse = bookMapper.toDto(bookService.getBookById(id));
+            BookResponse bookResponse = bookMapper.toDto(bookService.getById(id));
             return ResponseEntity.status(OK).body(bookResponse);
         } catch (EntityNotFoundException e) {
             return ResponseEntity.status(NOT_FOUND).build();
@@ -67,9 +72,25 @@ public class BookController {
     public ResponseEntity<BookResponse> updateById(@PathVariable("id") Long id,
                                                    @RequestBody CreateBookRequest bookDto) {
         Book book = bookMapper.toModel(bookDto);
-        book.setId(id);
-        Book updatedBook = bookService.update(book);
+        Book existingBook = bookService.getById(id);
+        Book updatedBook = bookService.update(id, updatedBookFields(existingBook, book));
         return ResponseEntity.status(OK).body(bookMapper.toDto(updatedBook));
+    }
+
+    private Consumer<Book> updatedBookFields(Book existingBook, Book book) {
+        return newBook -> {
+            Field[] fields = Book.class.getDeclaredFields();
+
+            for (Field field : fields) {
+                makeAccessible(field);
+                Object existingValue = getField(field, existingBook);
+                Object newValue = getField(field, book);
+
+                if (newValue != null && !newValue.equals(existingValue)) {
+                    setField(field, newBook, newValue);
+                }
+            }
+        };
     }
 
     @GetMapping("/search")
