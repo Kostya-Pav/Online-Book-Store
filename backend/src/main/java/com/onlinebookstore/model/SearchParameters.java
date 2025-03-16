d8e6201b397f86407fe87f1118864b937df9a8ff
Kostya-Pav <koast59385@gmail.com>
2025-03-16 19:15:55 +0200

    add corrections

--- backend/src/main/java/com/onlinebookstore/model/SearchParameters.java
+++ backend/src/main/java/com/onlinebookstore/model/SearchParameters.java
@@ -3,6 +3,5 @@ package com.onlinebookstore.model;
 import java.util.List;
 //Ми не можемо змінити назву поля або використовувати інверсію бо вона взаємозв'язана між назвою полів моделі
 //і назвами лістів
-// треба подумати над цим і подивитись в інтернеті альтернативний підхід для використання мультифільтрів
 public record SearchParameters(List<String> title, List<String> author, List<String> isbn) {
 }
